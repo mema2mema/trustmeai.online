e24eba6ceccca73ecb6e067a8170757cc709abaf
Mahesh TrustMe <mahesh@example.com>
2025-08-22 23:43:08 +0200

    we

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const db = new Database(path.join(__dirname, '..', 'data.sqlite'));
-db.pragma('journal_mode = WAL');
-export default db;
